5c1e116b5047cb12d19dae47980ce0975e1b69c3
abdofdl <abdofdl99@gmail.com>
2024-07-20 05:14:09 +0300

    no html and body

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,11 +1,5 @@
 import { IntlProviderHOC } from "../providors/IntlProvider";
 
 export default function Layout({ children, params: { locale } }: any) {
-  return (
-    <html lang={locale}>
-      <body>
-        <IntlProviderHOC locale={locale}>{children}</IntlProviderHOC>
-      </body>
-    </html>
-  );
+  return <IntlProviderHOC locale={locale}>{children}</IntlProviderHOC>;
 }
