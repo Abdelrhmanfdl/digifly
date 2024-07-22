0e85175b2b8aae955c1ced92b39aad3222e7f0e4
abdofdl <abdofdl99@gmail.com>
2024-07-22 07:57:02 +0300

    red icon in map

--- src/app/components/MapSection/index.tsx
+++ src/app/components/MapSection/index.tsx
@@ -11,26 +11,18 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import "leaflet-defaulticon-compatibility";
 
 import { MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet";
-import { icon, LatLngExpression } from "leaflet";
+import { LatLngExpression } from "leaflet";
 import L from "leaflet";
 import "@/app/components/MapSection/styles.css";
-import { useTranslations } from "next-intl";
 
-// const redIcon = new L.Icon({
-//   iconUrl:
-//     "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|FF0000",
-//   iconSize: [25, 41],
-//   iconAnchor: [12, 41],
-//   popupAnchor: [1, -34],
-//   shadowSize: [41, 41],
-// });
-
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl:
-//     "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
-//   icon: redIcon,
-//   shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
-// });
+const redIcon = new L.Icon({
+  iconUrl:
+    "https://icons.iconarchive.com/icons/paomedia/small-n-flat/512/map-marker-icon.png",
+  iconSize: [40, 40],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
 export default function Map({ position, tooltipContent }: MapComponentProps) {
   return (
@@ -39,7 +31,7 @@ export default function Map({ position, tooltipContent }: MapComponentProps) {
       zoom={18}
       maxZoom={18}
       scrollWheelZoom={true}
-      className="w-full min-h-96 bg-gradient-to-l"
+      className="w-full min-h-[28rem] bg-gradient-to-l"
       style={{
         backgroundImage:
           "linear-gradient(rgba(0,0,0,0) 0%, gray 10%, gray 90%, rgba(0,0,0,0)",
@@ -49,7 +41,7 @@ export default function Map({ position, tooltipContent }: MapComponentProps) {
         url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
+      <Marker position={position} icon={redIcon}>
         <Tooltip
           permanent
           direction="center"
