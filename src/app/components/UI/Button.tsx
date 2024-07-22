45970a9b2e8b0e38eae9b31da5c05b708fa7901f
abdofdl <abdofdl99@gmail.com>
2024-07-22 10:44:29 +0300

    darker green color

--- src/app/components/UI/Button.tsx
+++ src/app/components/UI/Button.tsx
@@ -11,7 +11,7 @@ export default function Button({ text, onClick, type }: ButtonProps) {
   return (
     <button
       type={type != undefined ? type : "button"}
-      className="w-full h-[52px] p-2 rounded-sm text-white font-[700] bg-digifly-green hover:bg-digifly-green-lighter cursor-pointer"
+      className="w-full h-[52px] p-2 rounded-sm text-white font-[700] bg-digifly-green hover:bg-[#3a976d] cursor-pointer"
       onClick={(e) => onClick?.(e)}
     >
       {text}
